896be4d740b902628f8ae0481b6aefb6d8e89f6c
Perfect <nobelperfect@gmail.com>
2026-04-27 13:47:09 -0700

    "Mac-Style" Glassmorphism & Animations

--- src/Code.js
+++ src/Code.js
@@ -1,5 +1,7 @@
 function doGet() {
-  return HtmlService.createHtmlOutputFromFile('Index')
-    .setTitle('Finance Dashboard')
+  return HtmlService.createTemplateFromFile('Index')
+    .evaluate()
+    .setTitle('Fund Dispatch Dashboard')
+    .addMetaTag('viewport', 'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0')
     .setXFrameOptionsMode(HtmlService.XFrameOptionsMode.ALLOWALL);
 }
